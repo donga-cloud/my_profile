ac881083120f10427baa93f025ffab2e84744e87
donga-cloud <wndbdk20@gmail.com>
2021-03-12 18:34:26 +0900

    first setting

---
+++ test.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<style>
+		/* CSS : Cascading Style Sheet */
+		h7{/* 태그 선택자 */
+			font-size:0.4em;
+			font-weight:bold;
+			display:block;
+		}
+		#header{/* id 선택자 */
+			background-color:orange;
+		}
+		#contents{
+			background-color:cyan;
+		}
+		.ccc{/* 클래스 선택자 */
+
+		}
+		</style>
+	</head>
+	<body>
+		<div id="header">
+			<h1>제목1</h1>
+			<h2>제목2</h2>
+			<h3>제목3</h3>
+			<h4>제목4</h4>
+			<h5>제목5</h5>
+			<h6>제목6</h6>
+			<h7>제목7</h7>
+			<h7>안녕하세요   ㄴㅇㄹ
+			여러분</h7>
+			<img src="main.png"/>
+			<a href="index1.php" >이전 페이지로가기</a>
+		</div>
+		<div id="contents">
+			컨텐츠 부분 입니다.
+			<ul>
+				<li>국문학 전공</li>
+				<li>국어 NLP 기반의 딥러닝 전공</li>
+				<li>언어기반 빅데이터 마이닝</li>
+				<li>SNS 빅데이터/ 인공지능 전문가</li>
+				<li>주력분야</li>
+				<li>	
+					<ol>
+						<li> TF기반 CNN,RNN,Custom DNN </li>
+						<li> 통계기반 데이터 마이닝 </li>
+						<li> XGboost,LGBM 전문 </li>
+						<li> DataWH 구축경험 SQL, RDBMS</li>
+						<li> 웹스크래이핑 활용 트렌드 분석 보고</li>
+						<li> Web App PM 및 유지보수 </li>
+
+					</ol>
+				</li>
+			</ul>
+		</div>
+		<div id="footer">
+			copyrights 2021 hong gil dong
+		</div>
+	</body>
+<html>
